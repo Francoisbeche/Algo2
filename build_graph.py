13773507721ee2312f639a2c67074f6ed4857cb7
francois <francois.beche@epitech.eu>
2020-01-18 17:20:58 +0100

    how to iterate

--- build_graph.py
+++ build_graph.py
@@ -4,5 +4,9 @@ netflix_file_path = './netflix_titles_nov_2019.csv'
 
 netflix_data = pd.read_csv(netflix_file_path)
 
-print(netflix_data);
+##print(netflix_data);
+def loopShit():
+    for index, row in netflix_data.iterrows():
+        print(row['show_id'], row['cast'])
 
+loopShit()
